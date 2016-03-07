f54a856a8d2a8e789c4291466b296b799ad0322e
xiliuhk <xiliuhk@gmail.com>
2016-03-07 00:27:25 -0500

    change q to statement to improve accuracy.

--- ans_pipeline.py
+++ ans_pipeline.py
@@ -8,6 +8,14 @@ import answer
 
 # wiki_path, question_path = sys.argv[1], sys.argv[2]
 
+def quest_to_state(q):
+   tokens = q.split(" ")
+   if tokens[0].startswith("Wh"):
+       return ' '.join(tokens[2:])
+   if tokens[0] == "How" and tokens[1] == "many":
+       return ' '.join(tokens[3:])
+   return ' '. join(tokens[1:])
+
 wiki_path, question_path = "test_wiki.htm", "test_quests.txt"
 
 sents = doc_parser.doc_to_sents(wiki_path)
@@ -18,10 +26,15 @@ with  open(question_path) as f:
     quests = f.read().splitlines()
 answers = []
 for q in quests:
-    q_vect = doc_parser.sent_to_vect(q)
+    q2 = quest_to_state(q)
+    q_vect = doc_parser.sent_to_vect(q2)
     ranked_sents = ans_ranker.rank_sents(q_vect, sents, sent_vects, sent_idfs)
     best = ranked_sents[0][0]
     q_tokens = tokenize.word_tokenize(q)
+    sys.stdout.write("Q: " + q + '\n')
+    # for rs in ranked_sents:
+    #     sys.stdout.write("A: " + rs[0] + '\n')
+
     ans = ""
     if q_tokens[0] == 'What':
         ans = answer.answer_what(q, best)
@@ -30,14 +43,24 @@ for q in quests:
     elif q_tokens[0] == 'Why':
         ans = answer.answer_why(q, best)
     elif q_tokens[0] == 'How many':
-        ans = answer.answer_how_many(q, best)
+        for sent in ranked_sents:
+            if len(filter(str.isdigit, sent)) > 0:
+                ans = answer.answer_how_many(q, sent)
+                break
     elif q_tokens[0] == 'Where':
         ans = answer.answer_where(q, best)
     elif q_tokens[0] == 'When':
-        ans = answer.answer_when(q, best)
+        #ans = answer.answer_when(q, best)
+        ans = answer.ans_when(q2, ranked_sents)
     else:
         ans = answer.answer_binary(q, best)
     answers.append(ans)
+    sys.stdout.write("A: " + (ans if len(ans)>0 else best) + '\n')
+    sys.stdout.write("----------\n")
 
-for ans in answers:
-    sys.stdout.write(ans+'\n')
+
+# for quest, ans in zip(quests, answers):
+#     sys.stdout.write("Q: " + quest + '\n')
+#     sys.stdout.write("A: " + ans + '\n')
+#     sys.stdout.write("----------\n")
+
